d66ac72c3dbf52cc472c273588cb38fd654f1483
os4ed <os4ed@os4eds-mac-mini.local>
2016-08-12 12:51:56 +0530

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by os4ed on 8/12/16.
+//  Copyright © 2016 os4ed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by os4ed on 8/12/16.
+//  Copyright © 2016 os4ed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
